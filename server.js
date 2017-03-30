71b7d87cb62f2fefd064090631760034423d9bc0
tohjg <tohjg85@gmail.com>
2017-03-30 22:17:43 +0800

    replace express with hapi

--- server.js
+++ server.js
@@ -1,29 +1,55 @@
-const express = require('express')
 const next = require('next')
+const Hapi = require('hapi')
+const Good = require('good')
+const { pathWrapper, defaultHandlerWrapper } = require('./next-wrapper')
 
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
 const app = next({ dir: '.', dev })
-const handle = app.getRequestHandler()
+const server = new Hapi.Server()
+
+// add request logging (optional)
+const pluginOptions = [
+  {
+    register: Good,
+    options: {
+      reporters: {
+        console: [{
+          module: 'good-console'
+        }, 'stdout']
+      }
+    }
+  }
+]
 
 app.prepare()
 .then(() => {
-  const server = express()
+  server.connection({ port: port })
+  server.register(pluginOptions)
+  .then(() => {
+    server.route({
+      method: 'GET',
+      path: '/a',
+      handler: pathWrapper(app, '/a')
+    })
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
+    server.route({
+      method: 'GET',
+      path: '/b',
+      handler: pathWrapper(app, '/b')
+    })
 
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
+    server.route({
+      method: 'GET',
+      path: '/{p*}', /* catch all route */
+      handler: defaultHandlerWrapper(app)
+    })
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+    server.start().catch(error => {
+      console.log('Error starting server')
+      console.log(error)
+    }).then(() => {
+      console.log('> Ready on http://localhost:'+port)
+    })
   })
 })
